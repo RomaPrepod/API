ef3a83781b7370f47c92181cb2d49d303e736243
Sergey Konkin <batareeed@gmail.com>
2017-01-21 23:43:59 +0300

    Added sidebar and base components stubs

--- RomaPrepod.WebApi/Startup.cs
+++ RomaPrepod.WebApi/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Net;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -12,6 +13,14 @@ namespace RomaPrepod.WebApi
 	{
 		private const string IndexPath = "/index.html";
 
+		private readonly string[] _angularRoutes =
+		{
+			"lectures",
+			"labs",
+			"tests",
+			"results"
+		};
+
 		public Startup(IHostingEnvironment env)
 		{
 			var builder = new ConfigurationBuilder()
@@ -23,12 +32,12 @@ namespace RomaPrepod.WebApi
 		}
 
 		public IConfigurationRoot Configuration { get; }
-		
+
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddMvc();
 		}
-		
+
 		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
 		{
 			loggerFactory.AddConsole(Configuration.GetSection("Logging"));
@@ -37,10 +46,9 @@ namespace RomaPrepod.WebApi
 			app.Use(async (context, next) =>
 			{
 				await next();
-				bool pageNotFound = context.Response.StatusCode == (int)HttpStatusCode.NotFound;
-				bool isApiPath = !context.Request.Path.Value.StartsWith("/api", StringComparison.OrdinalIgnoreCase);
 
-				if (pageNotFound && isApiPath)
+				if (context.Response.StatusCode == (int)HttpStatusCode.NotFound &&
+					_angularRoutes.Any(route => context.Request.Path.Value.StartsWith("/"+route, StringComparison.OrdinalIgnoreCase)))
 				{
 					context.Request.Path = IndexPath;
 					await next();
