80a5f69afc7fbe1bcb96d1a2754eace250aded22
Roman Shirokov <shirokovroma@gmail.com>
2017-01-24 13:34:11 +0700

    Text 4 lections

--- RomaPrepod.WebApi/Controllers/LecturesController.cs
+++ RomaPrepod.WebApi/Controllers/LecturesController.cs
@@ -17,21 +17,21 @@ namespace RomaPrepod.WebApi.Controllers
 				{
 					Title = "Лекция 1. Введение",
 					Date = DateTime.Today.AddDays(-2),
-					Description = "Occaecat proident exercitation eu aliquip irure ea quis ad dolor. Nulla cupidatat aute nisi enim eu ea minim labore nostrud mollit quis occaecat. Dolor aute do excepteur esse occaecat et aliqua. Aliqua aute tempor et ullamco labore Lorem excepteur. Commodo ut anim eu quis reprehenderit. Magna duis est veniam eiusmod exercitation non. Et deserunt enim duis pariatur irure id incididunt nisi."
+					Description = "В этой лекции студентов знакомят с тематикой предмета, предметной областью целями и задачами интернет приложений. Основной уроп идет на общее понимание среды существовония интренет проектов. Закрладывается фундамеент для дальнейшего освоения метриала курса. Так же на первом занятие предусмотрено входное тетирование, нацеленное на выяснение остаточных знаний студентов и общего уровня подготовки."
 				},
 
 				new Lecture
 				{
-					Title = "Лекция 2. Продолжение",
+					Title = "Лекция 2. Обзор сиситем управления контентом и знакомство с 1С-Битрикс",
 					Date = DateTime.Today.AddDays(-1),
-					Description = "Dolore fugiat ad esse cillum do sint veniam minim dolor ut ipsum et laboris. Anim proident duis Lorem anim voluptate. Ipsum amet voluptate cupidatat eiusmod ipsum laboris excepteur pariatur excepteur."
+					Description = "Лекция включает в себя обзор систем управления контентом существующих на рынке. Обзор функций и возможностей 1С-Битрикс:Управление сайтом и инфраскруктуры постороенной вокруг системы."
 				},
 
 				new Lecture
 				{
-					Title = "Лекция 3. Завершение",
+					Title = "Лекция 3. Внедрение шаблона дизайна.",
 					Date = DateTime.Today,
-					Description = "Tempor eiusmod proident et nulla nulla occaecat dolor pariatur aute nostrud. Eu sint officia id fugiat nulla non quis aute laborum. Proident cillum et mollit id nisi adipisicing ullamco consequat aliquip proident. Proident consequat ea aliqua ex. Cillum Lorem velit ut laborum veniam in ad amet tempor laborum nisi."
+					Description = "Рассматривается примение дизайна, правила интеграции гипертекстовой разметки в шаблон сайта построенного на 1С-Битрикс."
 				}
 			};
 		}
